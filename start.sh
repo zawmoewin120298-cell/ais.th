1a33099ffb34dbded85effee5d01ce17d976f26a
zawmoewin120298-cell <zawmoewin120298@gmail.com>
2026-09-26 15:20:13 +0700

    Refactor start.sh to use printf instead of echo
    Updated start.sh to use printf for script creation.

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
-# start.sh ဖန်တီးပုံကို ဒီလိုပြောင်းပါ
-RUN echo -e '#!/bin/sh\n/usr/local/bin/xray -config /etc/xray/config.json &\n/usr/local/openresty/bin/openresty -g "daemon off;"' > /start.sh \
+# start.sh ကို printf နဲ့ ဖန်တီးပါ (echo -e ထက် ပိုစိတ်ချရသည်)
+RUN printf '#!/bin/sh\n\
+/usr/local/bin/xray -config /etc/xray/config.json &\n\
+sleep 2\n\
+exec /usr/local/openresty/bin/openresty -g "daemon off;"\n' > /start.sh \
     && chmod +x /start.sh
 
 CMD ["/start.sh"]
